774cf984edaeffdec49f4662169624c7d7ce030b
Balaji <balaji.jayasankar@gmail.com>
2018-02-24 13:12:58 +0530

    Completed edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,15 +6,29 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 	def new
+        @article = Article.new
 	end
 	def edit
+        @article = Article.find(params[:id])
 	end
 	def create
-		@article = Article.new(params.require(:article).permit(:title, :text))
-		@article.save
-		redirect_to @article
+		@article = Article.new(article_params)
+		if @article.save
+		  redirect_to @article
+        else
+            render 'new'
+        end
 	end
 	def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+            redirect_to @article
+        else
+                Rails.logger.info(@article.errors.messages.inspect)
+            render 'edit'
+        end
+        
 	end
 	def destroy
 	end
